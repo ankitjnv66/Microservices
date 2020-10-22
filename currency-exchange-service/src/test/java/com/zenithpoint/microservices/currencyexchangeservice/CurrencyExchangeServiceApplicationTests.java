135f040bdf68a6a66f08853639be70f9a38108ff
Ankit Singh <ankitjnv66@gmail.com>
2020-10-22 15:29:50 +0530

    second commit

---
+++ currency-exchange-service/src/test/java/com/zenithpoint/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenithpoint.microservices.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
